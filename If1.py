155793ca5d1994ea4e5d0047de7bacc671ea85cf
GriKap <grikap@gmail.com>
2024-05-17 23:01:28 +0500

    Add files via upload

---
+++ If1.py
@@ -0,0 +1,22 @@
+x = 33
+if x > 0:
+    print('You got it')
+a, b = 3, 3
+if a >= b:
+    print('ok')
+if (a > b) or (a > 0):
+    print('good')
+if (a > b) and (a > 0 or b < 1000):
+    print('Wow')
+if 3 < b and b <10:
+    print('Can"t belive')
+if '45' > '235':
+    print('yes')
+if [1, 4, 6] > [2, 4, 6]:
+    print('good job')
+if '10' == 4:
+    print('Oh yeah')
+if [1, 3, 5] >= 5:
+    print('no')
+if '11' != 5:
+    print('amazing')
