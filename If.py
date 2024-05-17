825aa57b0332b3d964536d450046f58abe2ad611
GriKap <grikap@gmail.com>
2024-05-17 14:30:27 +0500

    Add files via upload

---
+++ If.py
@@ -0,0 +1,12 @@
+x = 33
+if x > 0:
+    print('You got it')
+a, b = 3, 3
+if a >= b:
+    print('ok')
+if (a > b) or (a > 0):
+    print('good')
+if (a > b) and (a > 0 or b < 1000):
+    print('Wow')
+if 3 < b and b <10:
+    print('Can"t belive')
